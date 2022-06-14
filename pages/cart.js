3212c3263572d470149911dad78f7b15fbaea380
Eric Boisvenu <burn4live@gmail.com>
2022-06-13 23:02:44 -0600

    refactor: minor fix

--- pages/cart.js
+++ pages/cart.js
@@ -13,51 +13,61 @@ export default function Cart() {
     <div className="grid grid-cols-1">
       <h1 className="text-4xl	text-center font-bold mt-5">Your Cart</h1>
       <div className="grid grid-cols-2 bg-white min-h-screen m-5 rounded-lg p-10">
-        <div className="flex flex-col">
-          {cart.map((product) => (
-            <div className="flex flex-col mt-5" key={product.id}>
-              <div className="flex flex-row">
-                <img className="w-48" src={product.image} alt="product image" />
-                <div className="flex flex-col pl-5">
-                  <span className="px-2 place-self-center text-4xl">
-                    {product.name}
-                  </span>
-                  <span className="px-2 py-2 text-xl">
-                    {product.description}
-                  </span>
-                  <span>
-                    Quantity:{" "}
-                    <span className="font-bold pl-2">{product.quantity}</span>
-                  </span>
-                  <span className="text-orange-700 font-bold">
-                    Price:{" "}
-                    <span className="text-black font-bold">
-                      {new Intl.NumberFormat("en-US", {
-                        style: "currency",
-                        currency: "CAD",
-                      }).format(product.price)}
+        {cart.length > 0 ? (
+          <div className="flex flex-col">
+            {cart.map((product) => (
+              <div className="flex flex-col mt-5" key={product.id}>
+                <div className="flex flex-row">
+                  <img
+                    className="w-48"
+                    src={product.image}
+                    alt="product image"
+                  />
+                  <div className="flex flex-col pl-5">
+                    <span className="px-2 place-self-center text-4xl">
+                      {product.name}
                     </span>
-                  </span>
-                </div>
-                <div className="pl-10">
-                  <button
-                    className="text-3xl rounded-full bg-orange-700 py-2 px-3 text-white hover:bg-orange-800"
-                    onClick={() => removeFromCart(product.id)}
-                  >
-                    &#x2715;
-                  </button>
+                    <span className="px-2 py-2 text-xl">
+                      {product.description}
+                    </span>
+                    <span>
+                      Quantity:{" "}
+                      <span className="font-bold pl-2">{product.quantity}</span>
+                    </span>
+                    <span className="text-orange-700 font-bold">
+                      Price:{" "}
+                      <span className="text-black font-bold">
+                        {new Intl.NumberFormat("en-US", {
+                          style: "currency",
+                          currency: "CAD",
+                        }).format(product.price)}
+                      </span>
+                    </span>
+                  </div>
+                  <div className="pl-10">
+                    <button
+                      className="text-3xl rounded-full bg-orange-700 py-2 px-3 text-white hover:bg-orange-800"
+                      onClick={() => removeFromCart(product.id)}
+                    >
+                      &#x2715;
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+        ) : (
+          <span className="justify-self-end">Your cart is empty</span>
+        )}
         <div>
-          <button
-            className="text-3xl rounded bg-orange-700 py-2 px-3 text-white hover:bg-orange-800"
-            onClick={() => handleCheckout(productsTotal)}
-          >
-            Checkout
-          </button>
+          {cart.length > 0 && (
+            <button
+              className="text-3xl rounded bg-orange-700 py-2 px-3 text-white hover:bg-orange-800"
+              onClick={() => handleCheckout(productsTotal)}
+            >
+              Checkout
+            </button>
+          )}
         </div>
       </div>
     </div>
