c1454aed3b99210c64ae334fc5db3269c5b16c0e
Eric Boisvenu <burn4live@gmail.com>
2022-06-19 16:00:08 -0600

    refactor: separated controls to accomadate product page

--- components/CartProduct.js
+++ components/CartProduct.js
@@ -1,12 +1,7 @@
 import React from "react";
-import { formatCurrency, safeRound } from "system/utils";
+import Controls from "./Controls";
 
-export default function CartProduct({
-  removeFromCart,
-  setQuantity,
-  product,
-  quantity,
-}) {
+export default function CartProduct({ product }) {
   return (
     <div className="flex flex-col mb-10" key={product.id}>
       <div className="flex place-items-center flex-col sm:flex-row">
@@ -14,49 +9,7 @@ export default function CartProduct({
         <div className="flex flex-col pl-5">
           <span className="font-bold text-xl sm:text-4xl">{product.name}</span>
           <span className="py-2 text-xl">{product.description}</span>
-          <div>
-            <label className="w-full text-gray-700 text-sm font-semibold">
-              Quantity
-            </label>
-            <div className="flex flex-row h-8 w-24 rounded-lg relative bg-transparent my-2">
-              <button
-                data-action="decrement"
-                onClick={() => setQuantity(quantity > 1 ? quantity - 1 : 1)}
-                className=" bg-orange-700 text-white hover:bg-orange-800 h-full w-20 rounded-l cursor-pointer outline-none"
-              >
-                <span className="m-auto text-2xl font-bold">−</span>
-              </button>
-              <span className="focus:outline-none text-center w-full place-self-center bg-gray-300 font-semibold text-md hover:text-black focus:text-black  md:text-basecursor-default text-gray-700  outline-none">
-                {quantity}
-              </span>
-              <button
-                data-action="increment"
-                onClick={() => setQuantity(quantity >= 1 ? quantity + 1 : 1)}
-                className="bg-orange-700 text-white hover:bg-orange-800 h-full w-20 rounded-l cursor-pointer outline-none"
-              >
-                <span className="m-auto text-2xl font-bold">+</span>
-              </button>
-            </div>
-          </div>
-          <span className="text-orange-700 font-bold">
-            Price:{" "}
-            <span className="text-black font-bold">
-              {formatCurrency(safeRound(product.price * product.quantity))}
-            </span>
-            {product.quantity > 1 && (
-              <div>
-                <span className="text-sm text-orange-900">
-                  ( {formatCurrency(product.price)} each )
-                </span>
-              </div>
-            )}
-          </span>
-          <button
-            className="text-sm rounded bg-orange-700 py-2 px-3 mt-2 text-white hover:bg-orange-800 w-36"
-            onClick={() => removeFromCart(product.id)}
-          >
-            Remove Item
-          </button>
+          <Controls isCart product={product} />
         </div>
       </div>
     </div>
---
+++ components/Controls.js
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from "react";
+import { useCart } from "hooks";
+import { formatCurrency, safeRound } from "system/utils";
+
+export default function Controls({ product, isCart }) {
+  const { cart, addToCart, updateQuantity, removeFromCart } = useCart();
+  const currentQuantity = cart.find((p) => p.id === product.priceId)?.quantity;
+  const [quantity, setQuantity] = useState(currentQuantity ?? 1);
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  useEffect(() => {
+    if (currentQuantity) updateQuantity(product.priceId, quantity);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [quantity]);
+
+  useEffect(() => {
+    setIsDisabled(Boolean(cart.find((p) => p.id === product.priceId)));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [cart]);
+
+  return (
+    <>
+      <div>
+        <label className="w-full text-gray-700 text-sm font-semibold">
+          Quantity
+        </label>
+        <div
+          className={`flex flex-row h-8 ${
+            isCart ? "w-24" : "w-full"
+          } rounded-lg relative bg-transparent my-2`}
+        >
+          <button
+            data-action="decrement"
+            onClick={() => setQuantity(quantity > 1 ? quantity - 1 : 1)}
+            className=" bg-orange-700 text-white hover:bg-orange-800 h-full w-20 rounded-l cursor-pointer outline-none"
+          >
+            <span className="m-auto text-2xl font-bold">−</span>
+          </button>
+          <span className="focus:outline-none text-center w-full place-self-center bg-gray-300 font-semibold text-md hover:text-black focus:text-black  md:text-basecursor-default text-gray-700  outline-none">
+            {quantity}
+          </span>
+          <button
+            data-action="increment"
+            onClick={() => setQuantity(quantity >= 1 ? quantity + 1 : 1)}
+            className="bg-orange-700 text-white hover:bg-orange-800 h-full w-20 rounded-l cursor-pointer outline-none"
+          >
+            <span className="m-auto text-2xl font-bold">+</span>
+          </button>
+        </div>
+        {isCart && (
+          <span className="text-orange-700 font-bold">
+            Price:{" "}
+            <span className="text-black font-bold">
+              {formatCurrency(safeRound(product.price * product.quantity))}
+            </span>
+            {product.quantity > 1 && (
+              <div>
+                <span className="text-sm text-orange-900">
+                  ( {formatCurrency(product.price)} each )
+                </span>
+              </div>
+            )}
+          </span>
+        )}
+      </div>
+
+      <div>
+        {isCart ? (
+          <button
+            className="text-sm rounded bg-orange-700 py-2 px-3 mt-2 text-white hover:bg-orange-800 w-36"
+            onClick={() => removeFromCart(product.id)}
+          >
+            Remove Item
+          </button>
+        ) : (
+          <button
+            disabled={isDisabled}
+            onClick={() => {
+              addToCart(product, quantity);
+            }}
+            className="disabled:bg-orange-900 font-bold w-full items-center py-2 px-3 text-sm text-center text-white bg-orange-700 rounded-lg hover:bg-orange-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
+          >
+            {isDisabled ? "Added To Cart" : "Add To Cart"}
+          </button>
+        )}
+      </div>
+    </>
+  );
+}
